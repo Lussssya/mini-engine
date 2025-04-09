0ba4f58cc70403922c2288493591d4f0e8bace5d
lusyasargsyan <lusya_sargsyan@edu.aua.am>
2025-04-09 12:15:55 +0400

    Hw2 without classes but cleaner

--- core/src/main/java/com/bootcamp/demo/pages/MissionsPage.java
+++ core/src/main/java/com/bootcamp/demo/pages/MissionsPage.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.bootcamp.demo.engine.Resources;
 import com.bootcamp.demo.engine.Squircle;
+import com.bootcamp.demo.engine.widgets.WidgetsList;
 import com.bootcamp.demo.pages.core.APage;
 
 public class MissionsPage extends APage {
@@ -48,17 +49,13 @@ public class MissionsPage extends APage {
     }
 
     private Table constructStatsSegment() {
-        final Table statsSegment = new Table();
-        statsSegment.pad(15).defaults().space(30);
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                final Table infoCell = new Table();
-                infoCell.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#504845")));
-                statsSegment.add(infoCell).growX().height(60);
-            }
-            if (row != 2) {
-                statsSegment.row();
-            }
+        final WidgetsList<Table> statsSegment = new WidgetsList<>(3);
+        statsSegment.pad(15).defaults().space(30).height(60).growX();
+
+        for (int i = 0; i < 9; i++) {
+            final Table infoCell = new Table();
+            infoCell.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#504845")));
+            statsSegment.add(infoCell);
         }
         return statsSegment;
     }
@@ -71,18 +68,20 @@ public class MissionsPage extends APage {
 
     // gear segment
     private Table constructEquipmentSegment() {
-        final Table table = new Table();
-        table.setBackground(Squircle.SQUIRCLE_35.getDrawable());
-        table.pad(30).defaults().space(30);
-        table.add(constructEquippedGearSegment()).grow();
-        table.add(constructSecondaryGearSegment()).grow();
-        return table;
+        final Table equipmentSegment = new Table();
+        equipmentSegment.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#f5eae3")));
+        equipmentSegment.pad(30).defaults().space(30);
+
+        equipmentSegment.add(constructEquippedGearSegment()).grow();
+        equipmentSegment.add(constructSecondaryGearSegment()).grow();
+        return equipmentSegment;
     }
 
     private Table constructEquippedGearSegment() {
         final Table gearSegment = new Table();
         gearSegment.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#d1cecc")));
         gearSegment.pad(30).defaults().space(30);
+
         gearSegment.add(constructIncompleteSet()).fillX().center().height(60);
         gearSegment.row();
         gearSegment.add(constructEquipment());
@@ -92,51 +91,63 @@ public class MissionsPage extends APage {
     private Table constructIncompleteSet() {
         final Table incompleteSet = new Table();
         incompleteSet.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#a9a29c")));
-        final Table button = new Table();
-        button.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#cfb6a3")));
-        incompleteSet.add(button).expandX().right().size(80);
+
+        incompleteSet.addActor(constructSetButtonActor());
         return incompleteSet;
     }
 
+    private Table constructSetButtonActor() {
+        final Table button = new Table();
+        button.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#cfb6a3")));
+
+        final Table layout = new Table();
+        layout.setFillParent(true);
+        layout.add(button).expand().right().size(80);
+        return layout;
+    }
+
     private Table constructEquipment() {
-        final Table gearSegment = new Table();
-        gearSegment.defaults().space(30);
-        for (int row = 0; row < 2; row++) {
-            for (int col = 0; col < 3; col++) {
-                final Table gear = new Table();
-                gear.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#b9a391")));
-                gearSegment.add(gear).size(200);
-            }
-            gearSegment.row();
+        final WidgetsList<Table> gearSegment = new WidgetsList<>(3);
+        gearSegment.defaults().space(30).size(200);
+
+        for (int i = 0; i < 6; i++) {
+            final Table gear = new Table();
+            gear.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#b9a391")));
+            gearSegment.add(gear);
         }
         return gearSegment;
     }
 
     private Table constructSecondaryGearSegment() {
-        final Table secondaryGearSegment = new Table();
         final Table firstColumn = new Table();
         firstColumn.defaults().space(30);
-        firstColumn.add(constructRelicSegment()).size(200).expand().top();
+        firstColumn.add(constructRelicSegment()).size(200);
         firstColumn.row();
-        firstColumn.add(constructFlagSegment()).size(200).expand().bottom();
-        secondaryGearSegment.pad(30).defaults().space(30);
-        secondaryGearSegment.add(firstColumn);
-        secondaryGearSegment.add(constructPetSegment()).growY().width(200).padTop(30).padBottom(30);
+        firstColumn.add(constructFlagSegment()).size(200);
+
+        final Table secondaryGearSegment = new Table();
+        secondaryGearSegment.pad(30).defaults();
+
+        final Table container = new Table();
+        container.defaults().space(30);
+        container.add(firstColumn);
+        container.add(constructPetSegment()).growY().width(200);
+        secondaryGearSegment.add(container);
+
         return secondaryGearSegment;
     }
 
     private Table constructRelicSegment() {
-        final Table relicSegment = new Table();
+        final WidgetsList<Table> relicSegment = new WidgetsList<>(2);
         relicSegment.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#c8c0b9")));
-        relicSegment.pad(20).defaults().space(20);
-        for (int row = 0; row < 2; row++) {
-            for (int col = 0; col < 2; col++) {
-                final Table relic = new Table();
-                relic.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#a9a29c")));
-                relicSegment.add(relic).grow();
-            }
-            relicSegment.row();
+        relicSegment.pad(20).defaults().space(20).grow();
+
+        for (int i = 0; i < 4; i++) {
+            final Table relic = new Table();
+            relic.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#a9a29c")));
+            relicSegment.add(relic);
         }
+
         return relicSegment;
     }
 
@@ -149,8 +160,10 @@ public class MissionsPage extends APage {
     private Table constructPetSegment() {
         final Table petSegment = new Table();
         petSegment.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#c8c0b9")));
+
         final Table button = new Table();
         button.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#debc7c")));
+
         petSegment.add(button).expand().bottom().growX().height(100);
         return petSegment;
     }
@@ -158,16 +171,19 @@ public class MissionsPage extends APage {
     // button segment
     private Table constructButtonSegment() {
         final Table buttonSegment = new Table();
-        buttonSegment.setBackground(Squircle.SQUIRCLE_35.getDrawable());
+        buttonSegment.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#f5eae3")));
         buttonSegment.pad(30).defaults().space(30).growX().height(150);
+
         final Table upgradeButton = new Table();
         upgradeButton.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#debc7c")));
+        buttonSegment.add(upgradeButton);
+
         final Table lootButton = new Table();
         lootButton.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#a6d890")));
+        buttonSegment.add(lootButton);
+
         final Table autoLootButton = new Table();
         autoLootButton.setBackground(Squircle.SQUIRCLE_35.getDrawable(Color.valueOf("#c2c2c2")));
-        buttonSegment.add(upgradeButton);
-        buttonSegment.add(lootButton);
         buttonSegment.add(autoLootButton);
 
         return buttonSegment;
