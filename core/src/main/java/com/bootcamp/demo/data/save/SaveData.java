e14503c1666f76a013055907969e531b0fa3ecb6
lusyasargsyan <lusya_sargsyan@edu.aua.am>
2026-06-17 19:26:12 +0400

    removed not used classes

--- core/src/main/java/com/bootcamp/demo/data/save/SaveData.java
+++ core/src/main/java/com/bootcamp/demo/data/save/SaveData.java
@@ -6,17 +6,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SaveData {
-    private TacticalsSaveData tacticalsSaveData = new TacticalsSaveData();
-
     private StatsSaveData statsSaveData = new StatsSaveData();
 
     private MilitaryGearsSaveData militariesSaveData = new MilitaryGearsSaveData();
 
     private AccessoryGearsSaveData accessoryGearsSaveData = new AccessoryGearsSaveData();
-
-    private PetsSaveData petsSaveData = new PetsSaveData();
-
-    private FlagsSaveData flagsSaveData = new FlagsSaveData();
 
     private SpecializationSaveData specializationSaveData = new SpecializationSaveData();
 }
